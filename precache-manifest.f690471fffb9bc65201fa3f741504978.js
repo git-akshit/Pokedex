e84e17ffa353dfee4d84f9bf9e6223fec7fd119d
07aki <akshitverma@Akshits-MacBook-Pro.local>
2020-02-23 14:46:06 +0530

    Updates

---
+++ precache-manifest.f690471fffb9bc65201fa3f741504978.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fda8b61af5d780cff83295578064d4",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "744b26d51a11c2f1fa98",
+    "url": "/Pokedex/static/css/main.17b59ff5.chunk.css"
+  },
+  {
+    "revision": "3de0495eebfe4749cc8f",
+    "url": "/Pokedex/static/js/2.8cc45f04.chunk.js"
+  },
+  {
+    "revision": "744b26d51a11c2f1fa98",
+    "url": "/Pokedex/static/js/main.0f46bca8.chunk.js"
+  },
+  {
+    "revision": "56efd2f60ead7c65ce96",
+    "url": "/Pokedex/static/js/runtime~main.c6077cef.js"
+  }
+]);
